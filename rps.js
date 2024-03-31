7f2d5a52c5983934eba789ef3c3e0688240f05ab
AWYCh <AWYCh@users.noreply.github.com>
2024-03-31 15:45:29 +0800

    Add playRound function that declares a winner or tie for a round

--- rps.js
+++ rps.js
@@ -5,12 +5,38 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    let input = prompt("Rock, Paper, Scissors?");
+    while (input == null) {
+        input = prompt("Rock, Paper, Scissors?");
+        if (input == false) {
+            continue;
+        }
+        if(options.includes(input)){
+            return input;
+        }
+    }
+    input = input.toLowerCase();
+
+}
 
 function playRound(playerSelection, computerSelection) {
-    //code here
+    if(playerSelection == computerSelection) {
+        return "It's a tie!";
+    }
+    else if((playerSelection == "rock" && computerSelection == "scissors") ||
+            (playerSelection == "paper" && computerSelection == "rock") ||
+            (playerSelection == "scissors" && computerSelection == "paper")
+){
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
 }
+else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+}
+}
+
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
+console.log(computerSelection)
 console.log(playRound(playerSelection, computerSelection));
